c3158981f458cc54a4d897b4642d687c596df1d5
Suhail <suhailcollegemail@gmail.com>
2024-01-03 14:49:15 +0530

    production complete

--- ctc-backend/sanity.cli.js
+++ ctc-backend/sanity.cli.js
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '61qsncth',
+    projectId: 'ask the owner bro',
     dataset: 'production'
   }
 })
